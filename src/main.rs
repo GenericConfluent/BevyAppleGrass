c652a98f9a10e470bf0f371de3c6dcff5bb88020
Matthew Power <m13power@gmail.com>
2025-05-27 17:42:54 -0600

    chore: finish configuring build stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
+fn say_hello() {
+    println!("Hello");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, say_hello)
+        .run();
 }
