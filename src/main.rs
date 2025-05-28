451b2e1fac275a756e9d514fcd0064c353ed4ac9
Matthew Power <m13power@gmail.com>
2025-05-27 21:27:40 -0600

    feat: player

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use bevy::prelude::*;
 
-fn say_hello() {
-    println!("Hello");
+#[derive(Component)]
+struct Player;
+
+fn spawn_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let rect = meshes.add(Rectangle::new(50.0, 50.0));
+    let mat = materials.add(Color::hsl(0.0, 1.0, 0.5));
+
+    commands.spawn((Mesh2d(rect), MeshMaterial2d(mat), Player));
+    commands.spawn(Camera2d);
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, say_hello)
+        .add_systems(Startup, spawn_player)
         .run();
 }
