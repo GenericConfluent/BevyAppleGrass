3241bbf3c5ad9a783a609ef8863460061cc13546
Matthew Power <m13power@gmail.com>
2025-05-30 21:50:37 -0600

    feat: start level loading

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use avian2d::PhysicsPlugins;
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 
 mod camera;
-mod player;
 mod enemy;
+mod player;
+
+fn load_world(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("map/world.ldtk").into(),
+        ..default()
+    });
+}
 
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(ImagePlugin::default_nearest()),
             PhysicsPlugins::default(),
+            LdtkPlugin,
             camera::CameraPlugin,
             player::PlayerPlugin,
             enemy::EnemyPlugin,
         ))
+        .insert_resource(LevelSelection::index(0))
+        .add_systems(Startup, load_world)
         .run();
 }
